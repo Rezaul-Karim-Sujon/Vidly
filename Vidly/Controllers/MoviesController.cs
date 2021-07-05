46ec50fd81b26bd7ac559edc1d275a0dc1e5f185
Rezaul-Karim-Sujon <rksujon126706@gmail.com>
2021-07-05 16:39:32 +0600

    Solution - Section 2 Exercise - Display the list of customers and movies.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -11,7 +11,20 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies/Random
-        public ActionResult Random()
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+        private IEnumerable<Movie>GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie{Id=1, Name="Shrek"},
+                new Movie{Id=2, Name="Wall-e"}
+            };
+        }
+        /*public ActionResult Random()
         {
             var movie = new Movie()
             {
@@ -29,7 +42,7 @@ namespace Vidly.Controllers
             };
             return View(viewModel);
             
-        }
+        }*/
         
     }
 }
