96e51ce98289fff3a975a14f975162230ea1af51
Rezaul-Karim-Sujon <rksujon126706@gmail.com>
2021-07-05 15:38:00 +0600

    Use a view model.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -16,16 +17,19 @@ namespace Vidly.Controllers
             {
                 Name = "Shrek!"
             };
-            return View(movie);
-            // return Content("Hello World");
-            // return HttpNotFound();
-            // return new EmptyResult();
-            // return RedirectToAction("Index", "Home",new{page=1, sortBy=name});
+            var customers = new List<Customer>
+            {
+                new Customer {Name="Customer 1"},
+                new Customer {Name="Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie=movie,
+                Customers=customers
+            };
+            return View(viewModel);
+            
         }
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year,int month)
-        {
-            return Content(year + "/" + month);
-        }
+        
     }
 }
